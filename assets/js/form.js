9916db0366320feff77b4dbabaee0f13078be1ea
Erik <rainer.erik70@gmail.com>
2024-04-01 10:02:36 -0700

    a singles change made i made a typo thats why was not working the submit button, now its working fine

---
+++ assets/js/form.js
@@ -0,0 +1,35 @@
+// Submit button start
+const username = document.querySelector(".username");
+const title = document.querySelector(".title");
+const content = document.querySelector(".content");
+const submitButton = document.querySelector(".submitButton");
+// submit button end
+
+// Starts Here the submit button function
+function saveForm(){
+    const userForm = {
+        username: username.value.trim(),
+        title: title.value.trim(),
+        content: content.value.trim()
+    };
+    localStorage.setItem("userForm", JSON.stringify(userForm));
+}
+
+function lastUser() {
+    const lastUser = JSON.parse(localStorage.getItem("userForm"));
+
+    if(lastUser !== null) {
+        document.querySelector(".username").innerHTML = userForm.username;
+        document.querySelector(".title").innerHTML = userForm.title;
+        document.querySelector(".content").innerHTML = userForm.content;
+        console.log(userForm);
+    }
+}
+
+submitButton.addEventListener("click", function(event){
+    event.preventDefault();
+    saveForm();
+    lastUser();
+});
+
+// End here the submit function
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,12 +2,6 @@
 const themeSwitcher = document.querySelector(".themeSwitcher");
 const container = document.querySelector(".container");
 // theme switcher end
-// Submit button start
-const username = document.querySelector(".username");
-const title = document.querySelector(".title");
-const content = document.querySelector(".content");
-const submitButton = document.querySelector("submitButton");
-// submit button end
 
 // Theme Mode Changes with localStorage, if theres no localStorage recorded yet
 // the initial mode will be "dark"
@@ -34,31 +28,3 @@ themeSwitcher.addEventListener("click", function() {
 });
 // Here ends the Theme Logic
 
-// Starts Here the submit button function
-function saveForm(){
-    const userForm = {
-        username: username.value.trim(),
-        title: title.value.trim(),
-        content: content.value.trim()
-    };
-    localStorage.setItem("userForm", JSON.stringify(userForm));
-}
-
-function lastUser() {
-    const lastUser = JSON.parse(localStorage.getItem("userForm"));
-
-    if(lastUser !== null) {
-        document.querySelector(".username").innerHTML = userForm.username;
-        document.querySelector(".title").innerHTML = userForm.title;
-        document.querySelector(".content").innerHTML = userForm.content;
-        console.log(userForm);
-    }
-}
-
-submitButton.addEventListener('click', function(event){
-    event.preventDefault();
-    saveForm();
-    lastUser();
-});
-
-// End here the submit function
