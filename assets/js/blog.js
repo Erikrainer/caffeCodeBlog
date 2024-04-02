6420485d7a03ef8e40ee1747419b4fab380f577c
Erik <rainer.erik70@gmail.com>
2024-04-02 09:11:38 -0700

    got the function working at least but its not doing what i want, a few updates on the blog.js almost there just a few adjusts away

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -4,15 +4,31 @@ const backButton = document.querySelector(".backButton");
 // Blog tags selection
 const blogList = document.querySelector(".blogList");
 const blogPost = document.querySelector(".blogPost");
-
+const blogPostList = document.querySelectorAll(".postList");
+const blogTitle = document.querySelectorAll(".blogTitle");
+const blogContent = document.querySelectorAll(".blogContent");
+const blogUser = document.querySelectorAll(".blogUser");
 let posts = [];
+let i = 0;
 // function to get back to the form
 backButton.addEventListener("click", function(event){
     window.location="index.html";
 });
 // End of function to get back to the form
+posts[i] = JSON.parse(localStorage.getItem("userForm"))
+// Start Here the Posts for the blog
+do{
+if(posts[i].username){
+    blogPostList[i].style.display = "none";
+    i++;
+}else {
+    blogTitle[i].textContent = posts[i].title;
+    blogContent[i].textContent = posts[i].content;
+    blogUser[i].textContent = posts[i].username;
+    i++;
 
-// Start Here the Posts for the blog 
-posts = JSON.parse(localStorage.getItem("userForm"));
+}
+}while(i < 4);
+i = 0;
 
 // Finish Here the Posts for the blog
